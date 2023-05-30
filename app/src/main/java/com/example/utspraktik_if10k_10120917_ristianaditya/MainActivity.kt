e8a492206064623cec07fcb6ef1984bca540303f
rstnadty <64267443+ristianaditya@users.noreply.github.com>
2023-05-30 19:51:02 +0700

    first commit

---
+++ app/src/main/java/com/example/utspraktik_if10k_10120917_ristianaditya/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if10k_10120917_ristianaditya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
